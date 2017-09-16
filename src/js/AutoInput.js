4d56631a5c3181e54aa82b97a881a2a9fe851241
Jimut Dhali <jemdhali67@gmail.com>
2017-09-17 01:58:16 +0530

    fixed issue of showing list when filter is empty

--- src/js/AutoInput.js
+++ src/js/AutoInput.js
@@ -21,17 +21,29 @@ const AutoInput = () => ({
 
     $scope.selectAsKey = (value) => {
       $scope.key = value;
-      $scope.showList = false;
+      $scope.displayList = false;
     };
 
     $scope.inputOnFocus = ($event) => {
-      $scope.showList = true;
+      $scope.displayList = true;
       $scope.focus($event);
     };
 
     $scope.inputOnBlur = ($event) => {
-      $scope.showList = false;
+      $scope.displayList = false;
       $scope.blur($event);
+    };
+
+    $scope.showSearchList = () => {
+      if (!$scope.displayList) {
+        return false;
+      }
+
+      if ($scope.values.filter($scope.search).length < 1) {
+        return false;
+      }
+
+      return true;
     };
 
     $scope.$watch('values', () => {
@@ -54,7 +66,7 @@ const AutoInput = () => ({
   template:
     '<div class="auto-input">' +
       '<input type="text" name="keyInput" placeholder="{{ placeholder }}" ng-model="key" ng-focus="inputOnFocus($event)" ng-blur="inputOnBlur($event)">' +
-      '<ul class="auto-input__list" ng-if="values.length && showList">' +
+      '<ul class="auto-input__list" ng-if="showSearchList()">' +
         '<li class="auto-input__list-item" ng-repeat="value in (values | filter:search)" ng-mousedown="selectAsKey(value)" ng-bind="value"></li>' +
       '</ul>' +
     '</div>',
