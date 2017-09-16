a7422d23a8d1040d4938778d261b8749922f8802
Jimut Dhali <jemdhali67@gmail.com>
2017-09-17 01:58:16 +0530

    fixed issue of showing list when filter is empty

--- src/js/AutoInput.js
+++ src/js/AutoInput.js
@@ -9,7 +9,7 @@ const AutoInput = () => ({
     blur: '&ngBlur',
   },
   link($scope, $elm, $attr, $ctrl) {
-    $scope.search = (v) => {
+    const search = (v) => {
       const index = v.search(new RegExp($scope.key, 'i'));
 
       if (index === 0) {
@@ -19,23 +19,46 @@ const AutoInput = () => ({
       return false;
     };
 
+    const filterValues = () => {
+      $scope.filteredValues = $scope.values.filter(search);
+    };
+
+    const resetKey = () => {
+      $scope.key = '';
+    };
+
     $scope.selectAsKey = (value) => {
       $scope.key = value;
-      $scope.showList = false;
+      $scope.displayList = false;
     };
 
     $scope.inputOnFocus = ($event) => {
-      $scope.showList = true;
+      $scope.displayList = true;
       $scope.focus($event);
     };
 
     $scope.inputOnBlur = ($event) => {
-      $scope.showList = false;
+      $scope.displayList = false;
       $scope.blur($event);
     };
 
+    $scope.showSearchList = () => {
+      if (!$scope.displayList) {
+        return false;
+      }
+
+      if ($scope.filteredValues.length < 1) {
+        return false;
+      }
+
+      return true;
+    };
+
+    $scope.$watch('key', filterValues);
+
     $scope.$watch('values', () => {
-      $scope.key = '';
+      filterValues();
+      resetKey();
     });
 
     // eslint-disable-next-line no-param-reassign
@@ -54,8 +77,8 @@ const AutoInput = () => ({
   template:
     '<div class="auto-input">' +
       '<input type="text" name="keyInput" placeholder="{{ placeholder }}" ng-model="key" ng-focus="inputOnFocus($event)" ng-blur="inputOnBlur($event)">' +
-      '<ul class="auto-input__list" ng-if="values.length && showList">' +
-        '<li class="auto-input__list-item" ng-repeat="value in (values | filter:search)" ng-mousedown="selectAsKey(value)" ng-bind="value"></li>' +
+      '<ul class="auto-input__list" ng-if="showSearchList()">' +
+        '<li class="auto-input__list-item" ng-repeat="value in filteredValues" ng-mousedown="selectAsKey(value)" ng-bind="value"></li>' +
       '</ul>' +
     '</div>',
 });
